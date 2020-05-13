51af369c1ef5f24fbe401ef33f8dc6bef1889842
Arthur <arthur.p.wright@gmail.com>
2020-05-13 19:20:34 -0400

    Initial commit

---
+++ api/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='api',
+   version='1.0.0',
+   description='api',
+   author='',
+   author_email='',
+   packages=['api'],
+   install_requires=[],
+   scripts=[
+           ]
+)
