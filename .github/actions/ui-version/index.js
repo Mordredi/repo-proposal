50f6f6fb81ed8842571b3379415a3460a38d4ca1
Arthur <arthur.p.wright@gmail.com>
2020-05-17 11:03:27 -0400

    get version

--- .github/actions/ui-version/index.js
+++ .github/actions/ui-version/index.js
@@ -1,16 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
+const fs = require('fs');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(process.env.GITHUB_WORKSPACE)
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const configPath = core.getInput('path');
+  const packageJSON = JSON.parse(fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, configPath, './package.json')));
+  const version = packageJSON.version;
+  core.setOutput('version', version)
+  console.log(`The version: ${version}`);
 } catch (error) {
   core.setFailed(error.message);
 }
