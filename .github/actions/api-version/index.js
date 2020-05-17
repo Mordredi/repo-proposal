217f3098590bc1fa605d0f8e39b7d7b8bb1a6619
Arthur <arthur.p.wright@gmail.com>
2020-05-17 11:39:54 -0400

    rebuild

--- .github/actions/api-version/index.js
+++ .github/actions/api-version/index.js
@@ -7,7 +7,7 @@ try {
   const configPath = core.getInput('path');
   const setupPy = fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, configPath, './setup.py')).toString();
   console.log(setupPy)
-  const version = setupPy.match('version\s*=\s*[\'"]([^\'"]*)[\'"]');
+  const version = setupPy.match(/version\s*=\s*[\'"]([^\'"]*)[\'"]/);
   if (version.length) {
     core.setOutput('version', version[0])
     console.log(`The version: ${version[0]}`);
