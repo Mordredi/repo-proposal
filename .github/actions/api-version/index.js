18e60807b9c267b55b355dcf57e2dc591648cf4c
Arthur <arthur.p.wright@gmail.com>
2020-05-17 11:25:05 -0400

    api version

---
+++ .github/actions/api-version/index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const path = require('path');
+const fs = require('fs');
+
+try {
+  const configPath = core.getInput('path');
+  const setupPy = fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, configPath, './setup.py')).toString();
+  const version = setupPy.search('version\s*=\s*[\'"]([^\'"]*)[\'"]');
+  core.setOutput('version', version)
+  console.log(`The version: ${version}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
