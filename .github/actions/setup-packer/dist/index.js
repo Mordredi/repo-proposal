6099fb0ab989754e58455a5e8608f203ac50ab72
Arthur <arthur.p.wright@gmail.com>
2020-05-18 11:29:12 -0400

    get tar

--- .github/actions/setup-packer/dist/index.js
+++ .github/actions/setup-packer/dist/index.js
@@ -48,12 +48,12 @@ module.exports =
 
 const core = __webpack_require__(684);
 const tc = __webpack_require__(404);
-const path = __webpack_require__(622);
-const fs = __webpack_require__(747);
+const exec = __webpack_require__(356);
 
 async function run() {
   try {
-    const packerPath = await tc.downloadTool('https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd64.zip');
+    exec.exec('aws --version');
+    const packerPath = await tc.downloadTool('https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd64.tar.gz');
     const packerPathExractedFolder = await tc.extractTar(packerPath);
     core.addPath(packerPathExractedFolder);
     console.log(packerPathExractedFolder);
--- .github/actions/setup-packer/index.js
+++ .github/actions/setup-packer/index.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
-const path = require('path');
-const fs = require('fs');
+const exec = require('@actions/exec');
 
 async function run() {
   try {
-    const packerPath = await tc.downloadTool('https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd64.zip');
+    exec.exec('aws --version');
+    const packerPath = await tc.downloadTool('https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd64.tar.gz');
     const packerPathExractedFolder = await tc.extractTar(packerPath);
     core.addPath(packerPathExractedFolder);
     console.log(packerPathExractedFolder);
